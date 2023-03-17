01313b371a2dc923405993acb34b7a1f4019e7ec
Francesco Punta <puntafrancesco@gmail.com>
2023-03-17 16:54:30 +0100

    Modifica nome file

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>Form per censura parola</title>
 </head>
 <body>
-	<form action="censura.php" method="post">
+	<form action="index.php" method="post">
 		<label for="paragrafo">Paragrafo:</label><br>
 		<textarea name="paragrafo" id="paragrafo" cols="35" rows="13"></textarea><br>
 		<label for="parola">Quale parola vuoi censurare?</label><br>
